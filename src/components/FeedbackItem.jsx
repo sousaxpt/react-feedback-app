d5bf678946465e1de1a06cd91ad26acc72c7c194
Pedro Sousa <emaildosousa@gmail.com>
2022-07-17 19:29:14 +0100

    Context
    Change from multiple useState to useContext

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,19 +1,25 @@
-import { FaTimes } from 'react-icons/fa'
-import PropTypes from 'prop-types'
+import { useContext } from 'react';
+import { FaTimes } from 'react-icons/fa';
+import PropTypes from 'prop-types';
 import Card from './shared/Card';
+import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackItem({ item, handleDelete }) {
+function FeedbackItem({ item }) {
+  const { deleteFeedback } = useContext(FeedbackContext);
+
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button onClick={() => handleDelete(item.id)} className="close"><FaTimes color='purple'/></button>
+      <button onClick={() => deleteFeedback(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
 }
 
 FeedbackItem.propTypes = {
-  item: PropTypes.object.isRequired
-}
+  item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
@@ -11,7 +12,28 @@ export const FeedbackProvider = ({ children }) => {
     },
   ]);
 
-  return <FeedbackContext.Provider value={{ feedback }}>{children}</FeedbackContext.Provider>;
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  return (
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        addFeedback,
+        deleteFeedback,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
 };
 
 export default FeedbackContext;
